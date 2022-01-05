6c284b1100d1e6207e43cef48cf0fc8fcf9461d6
minyoung <alsdud116@gmail.com>
2022-01-05 15:44:32 +0900

    자바 공부 기초부터 다시 시작!

---
+++ Java/src/ch02/ex01/C01Variable.java
@@ -0,0 +1,18 @@
+package ch02.ex01;
+
+public class C01Variable {
+	public static void main(String[] args) {
+		boolean bl = false;
+		
+		byte b = 0;
+		short s = 0;
+		char c = 0;
+		int i = 0;
+		long l = 0L;
+		
+		float f = 0.0f;
+		double d = 0.0;
+		
+		String str = "";
+	}
+}
